60ce7645553f13ee299a08592dffd4cb3bf585d9
Andrey Lantsov <andreyonohin88@gmail.com>
2024-08-06 15:36:56 +0300

    feat: deploy settings added

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,3 @@
+export const { PORT = 3000 } = process.env;
 export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 export const { DB_ADDRESS = 'mongodb://localhost:27017/mestodb' } = process.env;
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -6,6 +6,12 @@ import { validateObjId, validateAvatar, validateProfile } from '../middlewares/v
 
 const router = Router();
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0)
+});
+
 router.get('/me', getCurrentUser);
 router.get('/:id', validateObjId, getUser);
 router.patch('/me/avatar', validateAvatar, updateUserAvatar);
